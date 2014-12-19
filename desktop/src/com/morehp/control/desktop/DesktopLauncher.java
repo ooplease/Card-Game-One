446259e7f13ff9286adb512210ae6490b418cbb9
ooplease <ryanhornik@gmail.com>
2014-12-19 01:12:53 -0600

    Added libgdx

---
+++ desktop/src/com/morehp/control/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.morehp.control.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.morehp.control.GameController;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameController(), config);
+	}
+}
